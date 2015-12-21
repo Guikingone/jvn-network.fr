62c8bda565f031c458c6a9854e0812f0c8498953
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2015-12-21 19:23:06 +0100

    JVN-TEAM\\Mise à jour diverses//JVN-TEAM

--- src/UserBundle/Controller/SecurityController.php
+++ src/UserBundle/Controller/SecurityController.php
@@ -8,13 +8,11 @@ use Symfony\Component\Security\Core\SecurityContext;
 
 class SecurityController extends Controller
 {
-  public function loginAction(Request $request)
-  {
-    // Si le visiteur est déjà identifié, on le redirige vers l'accueil
+  public function loginAction(Request $request){
+    // Si utilisaeur authentifié, redirection vers accueil
     if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
       return $this->redirectToRoute('core_home');
     }
-
     // Le service authentication_utils permet de récupérer le nom d'utilisateur
     // et l'erreur dans le cas où le formulaire a déjà été soumis mais était invalide
     // (mauvais mot de passe par exemple)
