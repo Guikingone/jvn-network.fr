5b294800a65500d0eba7ac3c3aacd4de1c11c987
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2015-12-23 19:26:30 +0100

    JVN-TEAM\\Création formulaire Admin//JVN-TEAM

--- src/BlogBundle/Controller/AdminController.php
+++ src/BlogBundle/Controller/AdminController.php
@@ -5,6 +5,10 @@ use BlogBundle\Entity\Article;
 use BlogBundle\Entity\Commentaires;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class AdminController extends Controller{
 
@@ -12,17 +16,26 @@ class AdminController extends Controller{
   {
     // On crée un nouvel article
     $art = new Article();
-    $art->setTitre('Bonjour à tous !');
-    $art->setAuteur('Guikingone');
-    $art->setContenu('Bienvenue sur JVN-Network.fr, le site est tout neuf, bien entendu, je vous demande un peu de bon sens
-  et de compassion, ce n\'est qu\'un début');
-    $art->setCategorie('Annonce');
-    $art->setDatePublication(new \Datetime);
+    // On appelle le formulaire et on le fait hydrater $art
+    $formbuilder = $this->createFormBuilder($art)
+        ->add('titre', TextType::class)
+        ->add('auteur', TextType::class)
+        ->add('contenu', TextareaType::class)
+        ->add('datePublication', DateType::class)
+        ->add('categorie', TextType::class)
+        ->add('save', SubmitType::class, array('label' => 'Créer un article'))
+        ->getForm();
+        $formbuilder->handleRequest($request);
+        if($formbuilder->isValid()){
+          $em = $this->getDoctrine()->getManager();
+          $em->persist($art);
+          $em->flush();
+        }
+        // Un fois enregistré, on affiche une annonce flash de succés
+        $request->getSession()->getFlashBag()->add('success', "Annonce enregistré");
 
-    $em = $this->getDoctrine()->getManager();
-    $em->persist($art);
-    $em->flush();
-
-    return $this->render('BlogBundle::admin.html.twig');
+    return $this->render('BlogBundle::admin.html.twig', array(
+      'form' => $formbuilder->createView(),
+    ));
   }
 }
