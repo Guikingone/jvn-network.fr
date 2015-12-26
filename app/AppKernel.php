053b738c037c57d54eb74294b332792805fc473f
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2015-12-26 19:06:03 +0100

    JVN-TEAM\\ Ajout Bundle User + Sécurité//JVN-TEAM

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new CommuBundle\CommuBundle(),
             new UserBundle\UserBundle(),
             new BlogBundle\BlogBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
