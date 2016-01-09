53ece3429ddfa958daf68a86e9c6a295b8582e9c
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2016-01-09 15:44:09 +0100

    JVN-TEAM\\Mise en place Outils publication || Capifony//JVN-TEAM

---
+++ app/config/deploy.rb
@@ -0,0 +1,23 @@
+set :application, "set your application name here"
+set :domain,      "#{application}.com"
+set :deploy_to,   "/var/www/#{domain}"
+set :app_path,    "app"
+set :shared_files, ["app/config/parameters.yml"]
+set :shared_children, [app_path + "/logs", web_path + "/uploads", "vendor"]
+set :use_composer, true
+set :update_vendors, true
+
+set :repository,  "#{domain}:/var/repos/#{application}.git"
+set :scm,         :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+
+set :model_manager, "doctrine"
+# Or: `propel`
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+
+set  :keep_releases,  3
+
+# Be more verbose by uncommenting the following line
+# logger.level = Logger::MAX_LEVEL
