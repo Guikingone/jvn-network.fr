3178c35522f06335d92add34792ac4289494f925
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2016-01-09 19:05:53 +0100

    JVN-TEAM\\Mise en place Tchat||En cours//JVN-TEAM

--- src/CommuBundle/Controller/CommuController.php
+++ src/CommuBundle/Controller/CommuController.php
@@ -3,6 +3,8 @@
 namespace CommuBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CommuController extends Controller
 {
@@ -11,8 +13,15 @@ class CommuController extends Controller
         return $this->render('CommuBundle::index.html.twig');
     }
 
-    public function tchatAction()
+    public function tchatAction(Request $request)
     {
-      return $this->render('CommuBundle::index.html.twig');
+      $tchat = $this->getDoctrine()
+                    ->getManager()
+                    ->getRepository('CommuBundle:Tchat')
+                    ->getTchat();
+
+      return $this->render('CommuBundle:Tchat:index.html.twig', array(
+        'tchat' => $tchat
+      ));
     }
 }
