a128b2e1afb2d80cfaafe5905fef469914527ba9
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2016-01-28 19:16:09 +0100

    JVN-TEAM\\Suppression Capistrano et mise en place Capifony//JVN-TEAM

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,48 +1,19 @@
-# config valid only for current version of Capistrano
-lock '3.4.0'
+set :application, "set your application name here"
+set :domain,      "#{application}.com"
+set :deploy_to,   "/var/www/#{domain}"
+set :app_path,    "app"
 
-set :application, 'JVN-Network'
-set :repo_url, 'u83030699@jvn-network.fr:/git/.git/'
+set :repository,  "#{domain}:/var/repos/#{application}.git"
+set :scm,         :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :model_manager, "doctrine"
+# Or: `propel`
 
-# Default deploy_to directory is /var/www/my_app_name
- set :deploy_to, '/'
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
-# Default value for :scm is :git
-# set :scm, :git
+set  :keep_releases,  3
 
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
+# Be more verbose by uncommenting the following line
+# logger.level = Logger::MAX_LEVEL
