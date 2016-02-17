7762b3d3a1893414e3aaaf724910e99c11c956a1
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2016-02-17 10:32:04 +0100

    JVN-TEAM\\Mise en place bonne pratique Symfony || CSS/JS //JVN-TEAM

--- src/CoreBundle/Outils/Slug.php
+++ src/CoreBundle/Outils/Slug.php
@@ -6,7 +6,7 @@ class Slug {
 
   public function sluggify($string)
   {
-    return preg_replace('/[^a-z0-9]/', '-', strtolower(trim(strip_tags($string)));
+    return preg_replace('/[^a-z0-9]/', '-', strtolower(trim(strip_tags($string))));
     )
   }
 }
