ed0a76e31e952df2b67d74af8c8e2315ebffbcf3
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2016-05-03 09:16:21 +0200

    JVN-TEAM\\Ajout Express + Socket.io//JVN-TEAM

--- web/js/main.js
+++ web/js/main.js
@@ -1,5 +1,19 @@
 // Main.js //
 
+// Express
+
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
+// Jquery
 $(function(){
 
 });
