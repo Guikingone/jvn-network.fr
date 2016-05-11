bf244305779f796c261cb18c76050b744844313c
Guikingone <guillaume.loulier@hotmail.fr>
2016-05-11 10:07:48 +0200

    Core\\Modification Controller||En cours + Modification Vues||En cours//Core

---
+++ src/CoreBundle/Controller/blog/BlogController.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace CoreBundle\Controller\Blog;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use CoreBundle\Entity\Article;
+use CoreBundle\Entity\Commentaire;
+
+class BlogController extends Controller {
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/krma", name="krma")
+     * @Template("Blog\Krma\index.html.twig")
+     */
+    public function indexKrmaAction()
+    {
+      $article = $this->get('core.back')->index('KRMA');
+      return array('article' => $article);
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/membre", name="membre_blog")
+     * @Template("Blog\Membre\index.html.twig")
+     */
+    public function indexMembreAction()
+    {
+        $article = $this->get('core.back')->index('MEMBRE');
+        return array('article' => $article);
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/equipe", name="equipe")
+     * @Template("Blog\Team\index.html.twig")
+     */
+    public function indexEquipeAction()
+    {
+        $article = $this->get('core.back')->index('TEAM');
+        return array('article' => $article);
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/krma/admin", name="krma_admin")
+     * @Template("Blog\Krma\admin.html.twig")
+     */
+    public function adminKrmaAction(Request $request)
+    {
+        $article = $this->get('core.back')->index('KRMA');
+        $form = $this->get('core.back')->addArticle($request, 'KRMA');
+        return array('article' => $article,'form' => $form->createView());
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/membre/admin", name="membre_admin")
+     * @Template("Blog\Membre\admin.html.twig")
+     */
+    public function adminMembreAction(Request $request)
+    {
+        $article = $this->get('core.back')->index('MEMBRE');
+        $form = $this->get('core.back')->addArticle($request, 'MEMBRE');
+        return array('article' => $article, 'form' => $form->createView());
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/equipe/admin", name="equipe_admin")
+     * @Template("Blog\Team\admin.html.twig")
+     */
+    public function adminEquipeAction(Request $request)
+    {
+        $article = $this->get('core.back')->index('TEAM');
+        $form = $this->get('core.back')->addArticle($request, 'TEAM');
+        $membre = $this->getDoctrine()->getManager()->getRepository('UserBundle:User')->getUser();
+        $commentaire = $this->getDoctrine()->getManager()->getRepository('CoreBundle:Commentaire')->getCommentaires();
+        return array(
+            'article' => $article,
+            'form' => $form->createView(),
+            'membre' => $membre,
+            'commentaire' => $commentaire
+        );
+    }
+
+    /**
+     * @param Article $article
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/article/{id}/{slug}", name="home_view", requirements={"id": "\d+"})
+     * @Template("Blog\view.html.twig")
+     */
+    public function viewAction(Article $article, Request $request)
+    {
+        $form = $this->get('core.back')->viewArticle($request, $article);
+        $commentaire = $this->getDoctrine()
+                            ->getManager()
+                            ->getRepository('CoreBundle:Commentaire')
+                            ->findBy(array('article' => $article));
+        return array('article' => $article, 'commentaire' => $commentaire, 'form' => $form->createView());
+    }
+
+    /**
+     * @param Request $request
+     * @param Article $article
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @Route("/equipe/admin/update/{id}", name="equipe_update", requirements={"id": "\d+"})
+     * @Template("Blog\Membre\update.html.twig")
+     */
+    public function updateEquipeAction(Request $request, Article $article)
+    {
+        $form = $this->get('core.back')->updateArticle($request, $article);
+        $this->redirectToRoute('equipe_admin');
+        return array('form' => $form->createView(), 'article' => $article);
+    }
+    
+    /**
+     * @param Request $request
+     * @param Article $article
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @Route("/krma/admin/update/{id}", name="krma_update", requirements={"id": "\d+"})
+     * @Template("Blog\krma\update.html.twig")
+     */
+     public function updateKrmaAction(Request $request, Article $article)
+     {
+         $form = $this->get('core.back')->updateArticle($request, $article);
+         $this->redirectToRoute('krma_admin');
+         return array('form' => $form->createView(), 'article' => $article);
+     }
+
+    /**
+     * @param Request $request
+     * @param Article $article
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/membre/admin/update/{id}", name="membre_update", requirements={"id": "\d+"})
+     * @Template("Blog\Membre\update.html.twig")
+     */
+    public function updateMembreAction(Request $request, Article $article)
+    {
+        $form = $this->get('core.back')->updateArticle($request, $article);
+        $this->redirectToRoute('membre_admin');
+        return array('form' => $form->createView(), 'article' => $article);
+    }
+
+    /**
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @Route("/equipe/admin/delete/{id}", name="equipe_delete", requirements={"id": "\d+"})
+     */
+    public function deleteEquipeAction($id)
+    {
+        $this->get('core.back')->deleteArticle($id);
+        $this->addFlash('success', "L'article a été supprimé");
+        return $this->redirectToRoute('equipe_admin');
+    }
+
+    /**
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @Route("/krma/admin/delete/{id}", name="home_delete", requirements={"id": "\d+"})
+     */
+    public function deleteKrmaAction($id)
+    {
+        $this->get('core.back')->deleteArticle($id);
+        return $this->redirectToRoute('krma_admin');
+    }
+
+    /**
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @Route("/membre/admin/delete/{id}", name="membre_delete", requirements={"id": "\d+"})
+     */
+    public function deleteMembreAction($id)
+    {
+        $this->get('core.back')->deleteArticle($id);
+        $this->addFlash('success', "L'article a été supprimé");
+        return $this->redirectToRoute('membre_admin');
+    }
+}
