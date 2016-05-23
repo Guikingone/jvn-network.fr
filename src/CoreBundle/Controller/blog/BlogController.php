19fc41a195da85bb850942956bb1bdaefe679035
Guikingone <guillaume.loulier@hotmail.fr>
2016-05-23 11:19:23 +0200

    Core\\Modifications Vues||En cours + Modifications Controllers||En cours//Core

--- src/CoreBundle/Controller/blog/BlogController.php
+++ src/CoreBundle/Controller/blog/BlogController.php
@@ -10,7 +10,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use CoreBundle\Entity\Article;
 
 class BlogController extends Controller {
-
+    
     /**
      * @return \Symfony\Component\HttpFoundation\Response
      * @Route("/krma", name="krma")
