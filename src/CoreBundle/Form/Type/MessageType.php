a3d89b79151b5469b7a0b18d5964ecc4b4ca8d07
Guikingone <guillaume.loulier@hotmail.fr>
2016-06-21 13:51:29 +0200

    [!C !F !-C] || Corrections vues Forums|En cours

--- src/CoreBundle/Form/Type/MessageType.php
+++ src/CoreBundle/Form/Type/MessageType.php
@@ -3,9 +3,9 @@
 namespace CoreBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class MessageType extends AbstractType
@@ -17,7 +17,7 @@ class MessageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('contenu', TextareaType::class, array('required' => false))
+            ->add('contenu', TextType::class, array('required' => false))
             ->add('save', SubmitType::class)
         ;
     }
