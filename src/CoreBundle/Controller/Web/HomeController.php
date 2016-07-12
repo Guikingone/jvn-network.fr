9a1b6b811fa5b2449d4df54b5a1a585c31829d13
Guikingone <guillaume.loulier@hotmail.fr>
2016-07-12 09:28:53 +0200

    [+][!M] || Integration Maquettes|En cours + Modifications Services|En cours

--- src/CoreBundle/Controller/Web/HomeController.php
+++ src/CoreBundle/Controller/Web/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
      */
     public function indexAction()
     {
-      $article = $this->get('core.back')->index('KRMA', 'MEMBRE', 'TEAM');
+      $article = $this->get('core.back')->showArticles('KRMA', 'MEMBRE', 'TEAM');
       return array( 'article' => $article );
     }
 
